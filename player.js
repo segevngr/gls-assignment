8539e4ab7ed6992f810cd0728968b4594b864bb7
Segev <segevngr@gmail.com>
2021-12-28 00:59:01 +0200

    Bypass CORS with jsonp

--- player.js
+++ player.js
@@ -1,18 +1,23 @@
 function importJquery() {
     var script = document.createElement('script');
-    script.src = 'https://code.jquery.com/jquery-3.6.0.js';
     script.type = 'text/javascript';
-    document.getElementsByTagName('head')[0].appendChild(script);
+    script.src = 'https://code.jquery.com/jquery-3.6.0.js';
+    document.head.appendChild(script);
 }
 
-function loadJsonp() {
-    var guideUrl = 'https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=__5szm2kaj&refresh=true&env=dev&type=startPanel&vars%5Btype%5D=startPanel&sid=none&_=1582203987867'
-    $.getJSON(guideUrl, function(data) {
-        console.log(data)
-    })
+function getJsonp() {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=__5szm2kaj&refresh=true&env=dev&type=startPanel&vars%5Btype%5D=startPanel&sid=none&_=1582203987867';
+    document.head.appendChild(script);
 }
+
+const __5szm2kaj = function(data) {
+    console.log(data);
+} 
 
 window.runGls = function runGls() {
     importJquery()
-    loadJsonp()
+    getJsonp()
+    __5szm2kaj()
 }
