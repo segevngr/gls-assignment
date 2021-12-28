d08a427193e18a4af80dc140ce266d38444ab03d
Segev <segevngr@gmail.com>
2021-12-28 19:16:22 +0200

    Inject css, add wrapper,  empty tiplate & its css from guide

--- player.js
+++ player.js
@@ -1,23 +1,42 @@
-function importJquery() {
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://code.jquery.com/jquery-3.6.0.js';
-    document.head.appendChild(script);
+function loadGuide() {
+    const url = 'https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=getJsonpGuide&refresh=true&env=dev&type=startPanel&vars%5Btype%5D=startPanel&sid=none&_=1582203987867'
+    $('head').append(
+        '<script src=' + url + '></script>'
+    )
 }
 
-function getJsonp() {
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=__5szm2kaj&refresh=true&env=dev&type=startPanel&vars%5Btype%5D=startPanel&sid=none&_=1582203987867';
-    document.head.appendChild(script);
+function loadCSS() {
+    const url = 'https://guidedlearning.oracle.com/player/latest/static/css/stTip.css'
+    $('head').append(
+        '<link rel="stylesheet" type="text/css" href=' + url + '></link>'
+    )
 }
 
-const __5szm2kaj = function(data) {
-    console.log(data);
-} 
+function addGuideCss(css) {
+    $('head').append('<style class="guide-css"></style>')
+    $('.guide-css').html(css)
+}
+
+const getJsonpGuide = function(guide) {
+    addGuideCss(guide.data.css)
+    addTiplates(guide.data.tiplates)
+    // showFirstTooltip(guide.data.structure.steps)
+}
+
+function addTiplates(tiplates) {
+    console.log(tiplates)
+    $('body').append(
+        '<div  class="sttip">' +
+        '<div class="tooltip in">' +
+        '<div class="tooltip-arrow"></div>' +
+        '<div class="tooltip-arrow second-arrow"></div>' +
+        '<div class="popover-inner">' +
+        '</div></div></div>')
+    $('.popover-inner').html(tiplates.tip)
+    //$('.popover-inner').append(tiplates.hoverTip)
+}
 
 window.runGls = function runGls() {
-    importJquery()
-    getJsonp()
-    __5szm2kaj()
+    loadCSS()
+    loadGuide()
 }
