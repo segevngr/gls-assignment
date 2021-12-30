1118c51e06b3f12f25adfc4e835a4e3d83b788cc
Segev <segevngr@gmail.com>
2021-12-30 18:07:45 +0200

    Done with player.js: added jQueryUi Dialogs and set selectors locations

--- player.js
+++ player.js
@@ -1,5 +1,5 @@
 function loadGuide() {
-    const url = 'https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=getJsonpGuide&refresh=true&env=dev&type=startPanel&vars%5Btype%5D=startPanel&sid=none&_=1582203987867'
+    const url = 'https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=guideCallback&refresh=true&env=dev&type=startPanel&vars%5Btype%5D=startPanel&sid=none&_=1582203987867'
     $('head').append(
         '<script src=' + url + '></script>'
     )
@@ -13,8 +13,13 @@ function loadCSS() {
 }
 
 function addGuideCss(css) {
-    $('head').append('<style class="guide-css"></style>')
-    $('.guide-css').html(css)
+    $('head').append('<style class="guide-css"></style>');
+    $('.guide-css').html(css);
+
+    // For removing jQuery UI dialog default close btn
+    $('head').append(
+        '<style> .no-close .ui-dialog-titlebar-close {display: none;} </style>'
+    )
 }
 
 function addTipWrapper(tiplates) {
@@ -26,31 +31,70 @@ function addTipWrapper(tiplates) {
         '<div class="popover-inner">' +
         '</div></div></div>')
     $('.popover-inner').html(tiplates.tip)
-    //$('.popover-inner').append(tiplates.hoverTip)
+    $("button[data-iridize-role='closeBt']").click(function () {
+        $(".sttip").hide()
+    });
 }
 
-function loadTipContent(stepCount, steps) {
-    currStep = steps[stepCount]
-    if(currStep.action.type=="closeScenario")
-        $(".sttip").hide()
-    else {
-        content = currStep.action.contents['#content']
-        $("div[data-iridize-id=content]").html(content)
-        $("span[data-iridize-role=stepCount]").text(stepCount+1)
-        $("span[data-iridize-role=stepsCount]").text(steps.length-1)
-        $(".next-btn").click( function() {
-                loadTipContent(stepCount+1, steps)
-        })
+function setTipLocation(step) {
+    selector = $(step.action.selector);
+    if( !selector.length) {
+        selector = document.window;
+    }
+
+    $(".sttip").dialog({
+        dialogClass: "no-close",
+        position: { 
+            my: "top",
+            at: "bottom",
+            of: selector 
+        }
+    });
+}
+
+
+function setTipData(stepId, stepCount, steps) {
+    currStep = null;
+    for(let i=0; i<steps.length; i++){
+        if(steps[i].id == stepId) {
+            currStep = steps[i];
+        }
+    }
+
+    if (currStep.action.type == "closeScenario") {
+        $(".sttip").hide();
+        return;
+    }
+
+    content = currStep.action.contents['#content'];
+    $("div[data-iridize-id=content]").html(content);
+    $("span[data-iridize-role=stepCount]").text(stepCount);
+    $("span[data-iridize-role=stepsCount]").text(steps.length - 1);
+
+    setTipLocation(currStep);
+
+    nextStepId = currStep.followers[0].next;
+
+    $(".next-btn").unbind("click").click(function () {
+        setTipData(nextStepId, stepCount+1, steps);
+    });
+
+    if(typeof currStep.next !== 'undefined'){
+        nextSelector = currStep.next.selector
+        $(nextSelector).unbind("click").click(function () {
+            setTipData(nextStepId, stepCount+1, steps);
+        });
     }
 }
 
-const getJsonpGuide = function(guide) {
-    addGuideCss(guide.data.css)
-    addTipWrapper(guide.data.tiplates)
-    loadTipContent(0, guide.data.structure.steps)
+const guideCallback = function (guide) {
+    addGuideCss(guide.data.css);
+    addTipWrapper(guide.data.tiplates);
+    firstStepId = guide.data.structure.steps[0].id;
+    setTipData(firstStepId, 1, guide.data.structure.steps);
 }
 
 window.runGls = function runGls() {
-    loadCSS()
-    loadGuide()
+    loadCSS();
+    loadGuide();
 }
